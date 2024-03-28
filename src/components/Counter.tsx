5ed751a28a1b92bf010049ba766811eb0e05648b
SerjMan <Manuylov_serj@ukr.net>
2024-03-28 17:48:12 +0200

    Redux Toolkit Thunk and Slice

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,28 +1,29 @@
-import {useDispatch, useSelector} from 'react-redux'
-import {decrement, increment} from '../redux/actions'
-import { CounterState } from '../redux/types'
+import {useSelector} from 'react-redux'
+import {decrementAsync, incrementAsync} from '../redux/counterSlice'
+import {useAppDispatch, RootState} from '../redux/store'
 
 const Counter = () => {
-  const count = useSelector((state: CounterState) => state.count)
-  const dispatch = useDispatch()
+  const count = useSelector((state: RootState) => state.counter.count)
+  const dispatch = useAppDispatch()
 
   return (
     <div className='container d-flex flex-column align-items-center justify-content-center vh-100'>
-      <h1 className="mt-4 display-1">{count}</h1>
+      <h1 className='mt-4 display-1'>{count}</h1>
       <div className='mt-4 d-flex justify-content-center'>
         <button
           className='btn btn-danger me-4'
-          onClick={() => dispatch(increment())}
+          onClick={() => dispatch(incrementAsync())}
         >
           Increment
         </button>
         <button
           className='btn btn-primary'
-          onClick={() => dispatch(decrement())}
+          onClick={() => dispatch(decrementAsync())}
         >
           Decrement
         </button>
       </div>
+      <p className='mt-5 fs-3' >Redux Toolkit Thunk and Slice </p>
     </div>
   )
 }
